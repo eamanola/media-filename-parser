365064698e5f23bd13cf561f2286152a81251dda
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2024-06-15 20:16:01 +0300

    export unknowns

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-import title from './src/title.js';
-import year from './src/year.js';
-import season from './src/season.js';
-import episode from './src/episode.js';
+import title, { TITLE_UNKNOWN } from './src/title.js';
+import year, { YEAR_UNKNOWN } from './src/year.js';
+import season, { SEASON_UNKNOWN } from './src/season.js';
+import episode, { EPISODE_UNKNOWN } from './src/episode.js';
 
 export {
   title,
   year,
   season,
   episode,
+  TITLE_UNKNOWN,
+  YEAR_UNKNOWN,
+  SEASON_UNKNOWN,
+  EPISODE_UNKNOWN,
 };
 
 export default null;

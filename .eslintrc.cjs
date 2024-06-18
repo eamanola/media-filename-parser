2b74d063bc448f0fe6e2b795b4ebf699f418fbae
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2024-06-18 19:50:10 +0300

    clean eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,11 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
     jest: true,
+    node: true,
   },
   extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
   },
   rules: {
     'import/extensions': [0],

5e9687943e726af98f5906a3b2f7bc4d0c1101ce
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-06-12 22:24:37 +0300

    take 1st 2nd 3rd.. season

--- src/season.js
+++ src/season.js
@@ -3,8 +3,9 @@ import path from 'node:path';
 const SEASON_UNKNOWN = null;
 
 const re = /(?:season|s)\s*(?<season>\d+)/iu;
+const weak = /(?<season>\d+)(?:[^a-zA-Z1-9])*(?:st|nd|rd|th)(?:[^1-9a-zA-Z])*season/iu;
 
-const match = (filenamePart) => filenamePart.match(re);
+const match = (filenamePart) => filenamePart.match(re) || filenamePart.match(weak);
 
 const season = (filename) => {
   const parts = filename.split(path.sep).reverse();
